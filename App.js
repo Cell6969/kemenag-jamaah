6dd91120f675a0d7d649d2e369d6f3153f745e03
Cell6969 <bossmarinoo@gmail.com>
2023-09-12 14:26:42 +0700

    Update setTimeInterval for update position

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
-import Paho from 'paho-mqtt';
-import * as Location from 'expo-location';
+import React, { useState, useEffect } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import Paho from "paho-mqtt";
+import * as Location from "expo-location";
 
-const CLIENT_ID = 'Infinix';
+const CLIENT_ID = "Infinix";
 const client = new Paho.Client("p1a944b2.emqx.cloud", Number(8083), CLIENT_ID);
 
 const mqttUsername = "raka";
@@ -23,40 +23,39 @@ export default function App() {
   const getLocation = async () => {
     try {
       const { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
         return;
       }
-  
+
       const location = await Location.getCurrentPositionAsync({
         accuracy: Location.Accuracy.BestForNavigation, // Set the desired accuracy
         maximumAge: 0, // Set maximumAge to 0 to clear the cache
       });
-  
+
       setLatitude(location.coords.latitude);
       setLongitude(location.coords.longitude);
-  
+
       console.log(location.coords.latitude);
       console.log(location.coords.longitude);
       // Send location to MQTT broker
       sendLocation(location.coords.latitude, location.coords.longitude);
     } catch (error) {
-      console.error('Error getting location:', error);
-      setErrorMsg('Error getting location: ' + error.message);
+      console.error("Error getting location:", error);
+      setErrorMsg("Error getting location: " + error.message);
     }
   };
-  
 
   useEffect(() => {
     // Connect to MQTT broker
     client.connect({
       onSuccess: () => {
-        console.log('Connected to MQTT!');
-        client.subscribe('geo');
+        console.log("Connected to MQTT!");
+        client.subscribe("geo");
         client.onMessageArrived = onMessage;
       },
       onFailure: () => {
-        console.log('Failed to connect to MQTT!');
+        console.log("Failed to connect to MQTT!");
       },
       userName: mqttUsername,
       password: mqttPassword,
@@ -65,10 +64,16 @@ export default function App() {
     // Request location initially
     getLocation();
 
-    // Clean up MQTT subscription when the component unmounts
+    // Set up interval to update location every 1 second
+    const locationUpdateInterval = setInterval(() => {
+      getLocation();
+    }, 0);
+
+    // Clean up MQTT subscription and interval when the component unmounts
     return () => {
-      client.unsubscribe('geo'); // Unsubscribe from the MQTT topic
+      client.unsubscribe("geo"); // Unsubscribe from the MQTT topic
       client.disconnect(); // Disconnect from MQTT broker
+      clearInterval(locationUpdateInterval); // Clear the location update interval
     };
   }, []);
 
@@ -76,30 +81,20 @@ export default function App() {
   function sendLocation(latitude, longitude) {
     if (client.isConnected()) {
       const locationMessage = new Paho.Message(
-        JSON.stringify({ CLIENT_ID,latitude, longitude })
+        JSON.stringify({ CLIENT_ID, latitude, longitude })
       );
-      console.log( JSON.stringify({ CLIENT_ID,latitude, longitude }))
-      locationMessage.destinationName = 'geo'; // Change the destination topic as needed
+      locationMessage.destinationName = "geo"; // Change the destination topic as needed
       client.send(locationMessage);
     } else {
-      console.log('Not connected to the MQTT broker');
+      console.log("Not connected to the MQTT broker");
     }
   }
-
-  // Function to update location when the button is pressed
-  const updateLocation = () => {
-    getLocation();
-  };
 
   return (
     <View style={styles.container}>
       <Text>Latitude: {latitude}</Text>
       <Text>Longitude: {longitude}</Text>
       {errorMsg && <Text>{errorMsg}</Text>}
-      <Button
-        title="Update Location"
-        onPress={updateLocation} // Call updateLocation when the button is pressed
-      />
     </View>
   );
 }
@@ -107,8 +102,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
